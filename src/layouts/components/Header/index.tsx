ec353b597e0b3dc81633f0f2224d3ae926774dd9
phongTT <phongtt0701@gmail.com>
2023-08-13 13:38:51 +0700

    update logo import

--- src/layouts/components/Header/index.tsx
+++ src/layouts/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import logo from '~/assets/images/logo.svg'
+import logo from '~/common/assets/images/logo.svg'
 
 export default function Header() {
   const [navbar, setNavbar] = useState(false)
